ef9a0a341b63b9513095100d06db0b60d36b369d
Mark Kenneth Pelayo <mkgpdev@gmail.com>
2026-05-12 15:20:51 +0800

    init: v1.0.0 foundation

---
+++ src/routes/-navigation.ts
@@ -0,0 +1,9 @@
+import { linkOptions } from '@tanstack/react-router'
+
+export const homeLinkOptions = linkOptions({
+  to: '/',
+})
+
+export const aboutLinkOptions = linkOptions({
+  to: '/about',
+})
