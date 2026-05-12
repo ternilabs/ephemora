fb216d77795095ed78b7afbb0d2a14628b0812f7
Mark Kenneth Pelayo <mkgpdev@gmail.com>
2026-05-12 23:58:05 +0800

    chore(web): remove unused css and stale nav export

--- src/routes/-navigation.ts
+++ src/routes/-navigation.ts
@@ -3,7 +3,3 @@ import { linkOptions } from '@tanstack/react-router'
 export const homeLinkOptions = linkOptions({
   to: '/',
 })
-
-export const aboutLinkOptions = linkOptions({
-  to: '/about',
-})
